2a1697a8918077fb888e06a5a37d4e59c104e01d
mh-math-ai <minhae.pak@gmail.com>
2026-03-02 18:06:12 +0900

    structure of hardwares

---
+++ 0.1.c
@@ -0,0 +1,36 @@
+# 하드웨어의 구조 
+
+# 메인보드 
+
+중앙처리장치(CPU)
+Central Processing Unit
+
+
+주 기억장치(RAM)
+Primary Storage - Computer Memory
+Random-Access Memory 
+전원을 끄면 데이터가 사라짐 
+휘발성 Volatile type of memory 
+속도가 빠르기 때문 
+
+etc Read-Only Memory(ROM)
+
+그래픽스 카드 (GPU)
+
+Graphics Card
+예전에는 카드처럼 얇았다 
+
+Graphics Processing Unit
+요즘은 그래픽스 내부에 CPU가 들어있다 메모리역시 들어있다 
+
+보조 기억장치 (HDD, SSD)
+Secondary Storage
+
+Hard Disk Drive 
+
+Solid-State Drive
+USB가 발전한 형태 
+
+전통적으로 A, B가 플로피디스크 드라이브였다.. 그래서 지금 컴퓨터는 C 드라이브부터 시작한다
+
+그 전에는 자기 테이프(Magnetic Tape)에 저장하는 방식이 있었다 
