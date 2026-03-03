f6e9b30596a2bf71eef8d242f3c0f6f1852a8052
mh-math-ai <minhae.pak@gmail.com>
2026-03-03 19:31:10 +0900

    the structure of modern computers

---
+++ 0.5.c
@@ -0,0 +1,31 @@
+현대적 컴퓨터의 기본 구조
+
+예전에는 플러그보드, 펀치 카드를 사용했다 
+이걸 이용한 컴퓨터가 에니그마이다 
+
+내장형 프로그램 방식의 컴퓨터(Stored-program computer)
+
+폰 노이만 구조 
+input device 
+-> central processing unit(control unit + arithmetic/logic unit) + memory unit
+-> output device 
+
+
+현대 컴퓨터의 구조 
+
+Single System Computer Bus
+
+cpu    
+<-> Control bus (제어신호)
+-> Address bus (주소신호)
+<-> Data bus (데이터신호)
+
+memory
+<-> Control bus
+<- Address bus
+<-> data bus
+
+input and output (이 부분은 운영체제가 알아서 해줌)
+<-> Control bus
+<-> Address bus
+<-> Data bus
